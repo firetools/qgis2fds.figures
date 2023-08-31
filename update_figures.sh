e75bb878ad119ebcf8a914d7088ae3d6ec511ed9
Jonathan Hodges <johodges@vt.edu>
2023-08-31 16:03:48 -0400

    add cern_meyrin updated figures

--- update_figures.sh
+++ update_figures.sh
@@ -2,4 +2,6 @@ cp ../qgis2fds/verification/tests/golden_gate/FDS/*.png tests/golden_gate/FDS/
 cp ../qgis2fds/verification/tests/golden_gate/FDS/*.fds tests/golden_gate/FDS/
 cp ../qgis2fds/verification/tests/golden_gate/FDS/*.bingeom tests/golden_gate/FDS/
 
-
+cp ../qgis2fds/verification/tests/cern_meyrin/FDS/*.png tests/cern_meyrin/FDS/
+cp ../qgis2fds/verification/tests/cern_meyrin/FDS/*.fds tests/cern_meyrin/FDS/
+cp ../qgis2fds/verification/tests/cern_meyrin/FDS/*.bingeom tests/cern_meyrin/FDS/
