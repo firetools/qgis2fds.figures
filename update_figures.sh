fabaaa419ef8393a8cbb648350047f3a6e3be893
Jonathan Hodges <johodges@vt.edu>
2023-08-31 17:56:02 -0400

    update chimney_top_2 copy to be from firemodels_cad

--- update_figures.sh
+++ update_figures.sh
@@ -6,6 +6,6 @@ cp ../qgis2fds/verification/tests/cern_meyrin/FDS/*.png tests/cern_meyrin/FDS/
 cp ../qgis2fds/verification/tests/cern_meyrin/FDS/*.fds tests/cern_meyrin/FDS/
 cp ../qgis2fds/verification/tests/cern_meyrin/FDS/*.bingeom tests/cern_meyrin/FDS/
 
-cp ../qgis2fds/verification/tests/johodges_cad/Case_Studies/Chimney_Tops_2/FDS/*.png tests/Chimney_Tops_2/FDS/
-cp ../qgis2fds/verification/tests/johodges_cad/Case_Studies/Chimney_Tops_2/FDS/*.fds tests/Chimney_Tops_2/FDS/
-cp ../qgis2fds/verification/tests/johodges_cad/Case_Studies/Chimney_Tops_2/FDS/*.bingeom tests/Chimney_Tops_2/FDS/
+cp ../qgis2fds/verification/tests/firemodels_cad/Case_Studies/Chimney_Tops_2/FDS/*.png tests/Chimney_Tops_2/FDS/
+cp ../qgis2fds/verification/tests/firemodels_cad/Case_Studies/Chimney_Tops_2/FDS/*.fds tests/Chimney_Tops_2/FDS/
+cp ../qgis2fds/verification/tests/firemodels_cad/Case_Studies/Chimney_Tops_2/FDS/*.bingeom tests/Chimney_Tops_2/FDS/
