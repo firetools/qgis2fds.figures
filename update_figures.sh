374d5d308a8be505dbd167ebb2ed7857df44b5be
Jonathan Hodges <johodges@vt.edu>
2023-08-31 17:47:26 -0400

    Add chimney tops 2 figures

--- update_figures.sh
+++ update_figures.sh
@@ -5,3 +5,7 @@ cp ../qgis2fds/verification/tests/golden_gate/FDS/*.bingeom tests/golden_gate/FD
 cp ../qgis2fds/verification/tests/cern_meyrin/FDS/*.png tests/cern_meyrin/FDS/
 cp ../qgis2fds/verification/tests/cern_meyrin/FDS/*.fds tests/cern_meyrin/FDS/
 cp ../qgis2fds/verification/tests/cern_meyrin/FDS/*.bingeom tests/cern_meyrin/FDS/
+
+cp ../qgis2fds/verification/tests/johodges_cad/Case_Studies/Chimney_Tops_2/FDS/*.png tests/Chimney_Tops_2/FDS/
+cp ../qgis2fds/verification/tests/johodges_cad/Case_Studies/Chimney_Tops_2/FDS/*.fds tests/Chimney_Tops_2/FDS/
+cp ../qgis2fds/verification/tests/johodges_cad/Case_Studies/Chimney_Tops_2/FDS/*.bingeom tests/Chimney_Tops_2/FDS/
